4788cf9d49b7b6c1b07f2248f743cc023cd8cdfd
DurdaneAkca <akcadurdane2002@gmail.com>
2024-12-30 21:06:09 +0300

    Add project files.

---
+++ CSharpEgitimKampi601/Services/MongoDbConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace CSharpEgitimKampi601.Services
+{
+    public class MongoDbConnection
+    {
+        private IMongoDatabase _database;
+        public MongoDbConnection()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            _database = client.GetDatabase("Db601Customer");
+        }
+        public IMongoCollection<BsonDocument> GetCustomerCollection()
+        {
+            return _database.GetCollection<BsonDocument>("Customers");
+        }
+
+        internal object GetCustomersCollection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
